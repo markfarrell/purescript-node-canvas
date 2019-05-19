61873a73932008082041a988e2df9766c31050f8
Mark Farrell <m4farrel@uwaterloo.ca>
2019-05-19 23:57:28 +0000

    Revise tests

--- test/Main.purs
+++ test/Main.purs
@@ -19,17 +19,15 @@ import Node.Canvas as Canvas
 createCanvas' :: Number -> Number -> Aff CanvasElement
 createCanvas' width height = liftEffect $ Canvas.createCanvas width height
 
-getCanvasWidth' :: Number -> Number -> Aff Unit
-getCanvasWidth' width height =
+getCanvasWidth' :: Number -> CanvasElement -> Aff Unit
+getCanvasWidth' width canvas =
   do
-    canvas <- createCanvas' width height
     width' <- liftEffect $ Canvas.getCanvasWidth canvas
     Assert.equal width width'
 
-getCanvasHeight' :: Number -> Number -> Aff Unit
-getCanvasHeight' width height =
+getCanvasHeight' :: Number -> CanvasElement -> Aff Unit
+getCanvasHeight' height canvas =
   do
-    canvas <- createCanvas' width height
     height' <- liftEffect $ Canvas.getCanvasHeight canvas
     Assert.equal height height'
 
@@ -52,11 +50,11 @@ main :: Effect Unit
 main = runTest do
   suite "Node.Canvas" do
     test "getCanvasWidth" do
-      getCanvasWidth' 0.0 0.0
-      getCanvasWidth' 48.0 144.0
+      getCanvasWidth' 0.0 =<< createCanvas' 0.0 0.0
+      getCanvasWidth' 48.0 =<< createCanvas' 48.0 144.0
     test "getCanvasHeight" do
-      getCanvasHeight' 0.0 0.0
-      getCanvasHeight' 48.0 144.0
+      getCanvasHeight' 0.0 =<< createCanvas' 0.0 0.0
+      getCanvasHeight' 144.0 =<< createCanvas' 48.0 144.0
     test "loadImage" do
       loadImage' "public/images/characters/skin/1.png"
     test "toDataURL" do
