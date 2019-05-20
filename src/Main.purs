01abaadabbcad6ed89eb920169793ee8c8a5c9e5
Mark Farrell <m4farrel@uwaterloo.ca>
2019-05-20 17:48:41 +0000

    Args -> Data URIs

--- src/Main.purs
+++ src/Main.purs
@@ -4,36 +4,53 @@ import Prelude
 
 import Data.Array (drop) as Array
 import Data.Traversable (sequence)
+import Data.Tuple (curry, uncurry)
 
 import Effect (Effect)
-import Effect.Aff (launchAff)
+import Effect.Aff (Aff, launchAff)
 import Effect.Class (liftEffect)
 import Effect.Console (logShow)
 
 import Process (argv) as Process
 
-import Node.Canvas (CanvasImageSource)
+import Node.Canvas (CanvasElement, CanvasImageSource, Context2D)
 import Node.Canvas as Canvas
 
-args :: Effect (Array String)
-args = Array.drop 2 <$> Process.argv
+argv' :: Aff (Array String)
+argv' = liftEffect $ Array.drop 2 <$> Process.argv
 
-getDimensions :: CanvasImageSource -> Effect { width :: Number, height :: Number }
-getDimensions image = 
-  do
-    width' <- Canvas.getImageWidth image
-    height' <- Canvas.getImageHeight image 
-    pure { width : width',  height : height' }
+createCanvas' :: Number -> Number -> Aff CanvasElement
+createCanvas' = curry $ liftEffect <<< uncurry Canvas.createCanvas
+
+createCanvas'' :: CanvasImageSource -> Aff CanvasElement
+createCanvas'' image =
+  let
+    getImageWidth' = liftEffect <<< Canvas.getImageWidth
+    getImageHeight' = liftEffect <<< Canvas.getImageHeight
+    getContext2D' = liftEffect <<< Canvas.getContext2D
+  in
+    do
+      width <- getImageWidth' image
+      height <- getImageHeight' image
+      canvas <- createCanvas' width height
+      context <- getContext2D' canvas
+      _ <- drawImage' context image width height
+      pure canvas
+
+drawImage' :: Context2D -> CanvasImageSource -> Number -> Number -> Aff Unit
+drawImage' context image width height = liftEffect $ Canvas.drawImage context image 0.0 0.0 width height 0.0 0.0 width height
+
+toDataURL' :: CanvasElement -> Aff String
+toDataURL' = liftEffect <<< Canvas.toDataURL
 
 main :: Effect Unit
 main =
   let
-    args' = liftEffect $ args
-    getDimensions' = liftEffect <<< getDimensions
     logShow' = liftEffect <<< logShow
   in
     void $ launchAff $ do
-      sources <- args'
+      sources <- argv'
       images <- sequence $ Canvas.loadImage <$> sources
-      dimensions <- sequence $ getDimensions' <$> images
-      logShow' dimensions
+      canvases <- sequence $ createCanvas'' <$> images
+      urls <- sequence $ toDataURL' <$> canvases
+      logShow' urls
