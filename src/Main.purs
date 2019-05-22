866c87c5ce7b6be9143c7f5989df30f7a15ace02
Mark Farrell <m4farrel@uwaterloo.ca>
2019-05-22 18:21:37 +0000

    Layer images

--- src/Main.purs
+++ src/Main.purs
@@ -16,41 +16,30 @@ import Process (argv) as Process
 import Node.Canvas (CanvasElement, CanvasImageSource, Context2D)
 import Node.Canvas as Canvas
 
-argv' :: Aff (Array String)
-argv' = liftEffect $ Array.drop 2 <$> Process.argv
-
 createCanvas' :: Number -> Number -> Aff CanvasElement
 createCanvas' = curry $ liftEffect <<< uncurry Canvas.createCanvas
 
-createCanvas'' :: CanvasImageSource -> Aff CanvasElement
-createCanvas'' image =
-  let
-    getImageWidth' = liftEffect <<< Canvas.getImageWidth
-    getImageHeight' = liftEffect <<< Canvas.getImageHeight
-    getContext2D' = liftEffect <<< Canvas.getContext2D
-  in
-    do
-      width <- getImageWidth' image
-      height <- getImageHeight' image
-      canvas <- createCanvas' width height
-      context <- getContext2D' canvas
-      _ <- drawImage' context image width height
-      pure canvas
+drawImage' :: Context2D -> Number -> Number -> CanvasImageSource -> Aff Unit
+drawImage' context width height image = liftEffect $ Canvas.drawImage context image 0.0 0.0 width height 0.0 0.0 width height
 
-drawImage' :: Context2D -> CanvasImageSource -> Number -> Number -> Aff Unit
-drawImage' context image width height = liftEffect $ Canvas.drawImage context image 0.0 0.0 width height 0.0 0.0 width height
+layer :: Number -> Number -> Array CanvasImageSource -> Aff CanvasElement
+layer width height images =
+  do
+    canvas <- createCanvas' width height
+    context <- liftEffect $ Canvas.getContext2D canvas
+    _ <- sequence $ drawImage' context width height <$> images
+    pure canvas
 
-toDataURL' :: CanvasElement -> Aff String
-toDataURL' = liftEffect <<< Canvas.toDataURL
+layer' :: Number -> Number -> Array String -> Aff String
+layer' width height sources =
+  do
+    images <- sequence $ Canvas.loadImage <$> sources
+    canvas <- layer width height $ images
+    liftEffect $ Canvas.toDataURL canvas
 
 main :: Effect Unit
 main =
-  let
-    logShow' = liftEffect <<< logShow
-  in
-    void $ launchAff $ do
-      sources <- argv'
-      images <- sequence $ Canvas.loadImage <$> sources
-      canvases <- sequence $ createCanvas'' <$> images
-      urls <- sequence $ toDataURL' <$> canvases
-      logShow' urls
+  void $ launchAff $ do
+    sources <- liftEffect $ Array.drop 2 <$> Process.argv
+    dataURL <- layer' 48.0 144.0 sources
+    liftEffect $ logShow dataURL
