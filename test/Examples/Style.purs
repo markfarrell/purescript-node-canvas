d0bf2efc9f0fad77cb05b9f66030c164198f5ee5
Mark Farrell <m4farrel@uwaterloo.ca>
2019-05-27 16:39:08 +0000

    Test.Examples.Style <- Process Args

--- test/Examples/Style.purs
+++ test/Examples/Style.purs
@@ -2,10 +2,12 @@ module Test.Examples.Style where
 
 import Prelude
 
+import Data.Maybe (Maybe)
 import Data.Array (drop) as Array
 import Data.Traversable (sequence) as Traversable
 
 import Effect (Effect)
+import Effect.Aff (Aff)
 import Effect.Aff (launchAff) as Aff
 import Effect.Console (logShow) as Console
 import Effect.Class (liftEffect) as Effect
@@ -14,22 +16,19 @@ import Node.Process (argv) as Process
 
 import Node.Canvas.Aff as Canvas
 
+import Test.Style (Style)
 import Test.Style (fromArray) as Style
 
-main :: Effect Unit
-main =
+sampleStyle :: String -> Aff (Maybe Style)
+sampleStyle style =
   let
-    argv' = Array.drop 2 <$> Process.argv
-    logShow' = Effect.liftEffect <<< Console.logShow
     x = 0.0
     y = 0.0
     width = 1.0
     height = 1.0
-    style = "red"
     indices = [0, 1, 2, 3]
   in
-    void $ Aff.launchAff $ do
-      sources <- Effect.liftEffect $ argv'
+    do
       canvas <- Canvas.createCanvas width height
       context <- Canvas.getContext2D canvas
       _ <- Canvas.setStrokeStyle context style
@@ -39,5 +38,15 @@ main =
       _ <- Canvas.fillRect context x y width height 
       imageData <- Canvas.getImageData context x y width height
       result <- Traversable.sequence $ Canvas.getImageDataIndex imageData <$> indices
-      result' <- pure $ join $ Style.fromArray <$> Traversable.sequence result
-      logShow' $ result'
+      pure $ join $ Style.fromArray <$> Traversable.sequence result
+
+main :: Effect Unit
+main =
+  let
+    argv' = Effect.liftEffect $ Array.drop 2 <$> Process.argv
+    logShow' = Effect.liftEffect <<< Console.logShow 
+  in
+    void $ Aff.launchAff $ do
+      styles <- argv'
+      result <- Traversable.sequence $ sampleStyle <$> styles
+      logShow' $ Traversable.sequence result 
